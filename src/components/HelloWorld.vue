a87a93f8412c1bdfdaf373d51aeed8374ba33d5b
Hittin Yann <yhittin@bar.sio.fr>
2021-03-11 11:01:11 +0100

    update home page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,15 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      En 2021, j'était en 2ème année de BTS SIO option SLAM.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <p>
+      <button type='button' v-on:click="this.$router.push({ path: '/about' })" class='home-b'>About</button>
+    </p>
+    <h2>Réseau sociaux</h2>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">Facebook</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">Instagram</a></li>
     </ul>
   </div>
 </template>
@@ -39,20 +24,39 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
+<style type='text/scss' scoped>
+h2 {
   margin: 40px 0 0;
 }
 ul {
   list-style-type: none;
   padding: 0;
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+
+    a {
+      color: #42b983;
+    }
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.home-b {
+  border: 0;
+    line-height: 2.5;
+    padding: 0 20px;
+    font-size: 1rem;
+    text-align: center;
+    color: black;
+    border-radius: 10px;
+    background-color: #33CEFF;
+    background-image: linear-gradient(to top left,
+                                      rgba(0, 0, 0, .2),
+                                      rgba(0, 0, 0, .2) 30%,
+                                      rgba(0, 0, 0, 0));
+    box-shadow: inset 2px 2px 3px rgba(255, 255, 255, .6),
+                inset -2px -2px 3px rgba(0, 0, 0, .6);
 }
-a {
-  color: #42b983;
-}
+
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="logoG" src="../assets/Mortel-modifier.png">
+    <HelloWorld msg="Bienvenue sur le site de Geronde"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+img {
+  width: 20%;
+  height: auto;
+}
+</style>
